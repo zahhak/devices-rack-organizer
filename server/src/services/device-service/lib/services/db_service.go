abcf88f12aee8e37d49edd93ed699a5f4bbff2c3
George Yanev <george.yanev@telerik.com>
2018-06-08 10:15:03 +0300

    First working GetAll

--- server/src/services/device-service/lib/services/db_service.go
+++ server/src/services/device-service/lib/services/db_service.go
@@ -1,6 +1,15 @@
 package services
 
-import "github.com/zahhak/devices-rack-organizer/server/src/services/device-service/lib/types/db"
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/zahhak/devices-rack-organizer/server/src/services/device-service/lib/types/db"
+)
 
 // DBService ...
 type DBService struct {
@@ -8,5 +17,26 @@ type DBService struct {
 
 // GetAllDevices returns all devices stored in the database.
 func (service *DBService) GetAllDevices() ([]db.Device, error) {
-	return []db.Device{}, nil
+	sess := session.New()
+	client := dynamodb.New(sess)
+	dbInput := &dynamodb.ScanInput{
+		TableName: aws.String("RackOrganizer"),
+	}
+	allItems, err := client.Scan(dbInput)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	devices := []db.Device{}
+	device := new(db.Device)
+	for _, Item := range allItems.Items {
+		err = dynamodbattribute.UnmarshalMap(Item, device)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		devices = append(devices, *device)
+		fmt.Println(devices)
+	}
+	return devices, nil
 }
